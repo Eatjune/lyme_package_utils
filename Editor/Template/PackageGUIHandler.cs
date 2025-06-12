3fe7e393f5a6ede083c61c1d86cd7d2af5b037ba
Eatjune <490359426@qq.com>
2025-06-12 12:09:07 +0800

    - 修改readme使用方法 - 修改模板，注释掉了

--- Editor/Template/PackageGUIHandler.cs
+++ Editor/Template/PackageGUIHandler.cs
@@ -1,8 +1,8 @@
-﻿namespace LymeGame.Utils.Package {
-# if UNITY_EDITOR
-	public static partial class PackageGUIHandler {
-		public static void GetGUIHandler(string searchContext) {
-		}
-	}
-#endif
-}
+﻿// namespace LymeGame.Utils.Package {
+// # if UNITY_EDITOR
+// 	public static partial class PackageGUIHandler {
+// 		public static void GetGUIHandler(string searchContext) {
+// 		}
+// 	}
+// #endif
+// }
